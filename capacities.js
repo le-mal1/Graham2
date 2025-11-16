146536ade87ffc7f259a40f9b7a741ff6805acde
le-mal1 <you@example.com>
2025-11-16 16:28:51 +0100

    typo

--- capacities.js
+++ capacities.js
@@ -2,7 +2,7 @@ const TRIGGER_START_YOUR_TURN = "start your turn";
 const TRIGGER_START_OPPONENT_TURN = "start opponent turn";
 const EFFECT_ADD_FORCE_LEADER = "add force leader";
 const EFFECT_ADD_PV_LEADER = "add pv leader";
-const EFFECT_CALL_SUPPORT = "call suport";
+const EFFECT_CALL_SUPPORT = "call support";
 
 const TRIGGERS_ARRAY = [
     TRIGGER_START_YOUR_TURN,
