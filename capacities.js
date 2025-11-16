6d49a0cfb84dd2e22683ff1a8ebd2d933794df1c
le-mal1 <you@example.com>
2025-11-16 19:53:32 +0100

    add target constants

--- capacities.js
+++ capacities.js
@@ -1,10 +1,31 @@
 const TRIGGER_START_YOUR_TURN = "start your turn";
 const TRIGGER_START_OPPONENT_TURN = "start opponent turn";
+
+// Ancient effects
 const EFFECT_ADD_FORCE_1_LEADER = "add force 1 leader";
 const EFFECT_ADD_FORCE_1_CARD = "add force 1 card";
 const EFFECT_ADD_PV_1_LEADER = "add pv 1 leader";
 const EFFECT_ADD_PV_1_CARD = "add pv 1 card";
 const EFFECT_CALL_SUPPORT = "call support";
+
+// new effects
+const EFFECT_ADD_FORCE = "add force";
+
+const TARGET_CARD = "target card";
+const TARGET_MY_LEADER = "target my leader";
+const TARGET_OPPONENT_LEADER = "target opponent leader";
+const TARGET_MY_LEADER_NEIGHBOORS = "target my leader neighboors";
+const TARGET_OPPONENT_LEADER_NEIGHBOORS = "target opponent leader neighboors";
+const TARGET_MY_EDGE_RIGHT = "target my edge right";
+const TARGET_MY_EDGE_LEFT = "target my edge left";
+const TARGET_OPPONENT_EDGE_RIGHT = "target opponent edge right";
+const TARGET_OPPONENT_EDGE_LEFT = "target opponent edge left";
+const TARGET_MY_EDGES = "target my edges";
+const TARGET_OPPONENT_EDGES = "target opponent edges";
+const TARGET_MY_CARDS = "target my cards";
+const TARGET_OPPONENT_CARDS = "target opponent cards";
+const TARGET_EVERY_CARDS = "target every cards";
+
 
 const TRIGGERS_ARRAY = [
     TRIGGER_START_YOUR_TURN,
@@ -17,4 +38,21 @@ const EFFECTS_ARRAY = [
     EFFECT_ADD_FORCE_1_CARD,
     EFFECT_ADD_PV_1_CARD,
     EFFECT_CALL_SUPPORT,
+];
+
+const TARGETS_ARRAY = [
+    TARGET_CARD,
+    TARGET_MY_LEADER,
+    TARGET_OPPONENT_LEADER,
+    TARGET_MY_LEADER_NEIGHBOORS,
+    TARGET_OPPONENT_LEADER_NEIGHBOORS,
+    TARGET_MY_EDGE_RIGHT,
+    TARGET_MY_EDGE_LEFT,
+    TARGET_OPPONENT_EDGE_RIGHT,
+    TARGET_OPPONENT_EDGE_LEFT,
+    TARGET_MY_EDGES,
+    TARGET_OPPONENT_EDGES,
+    TARGET_MY_CARDS,
+    TARGET_OPPONENT_CARDS,
+    TARGET_EVERY_CARDS,
 ];
